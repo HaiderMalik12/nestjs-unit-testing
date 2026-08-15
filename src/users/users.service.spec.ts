839d585ba28f52d42c16e434e11fe5163bde991f
Haider Malik <haidermalik504@gmail.com>
2026-08-15 16:01:33 +0500

    delete user test cases

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,30 +2,74 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
-   
   it('should create a new user', () => {
-
     const service = new UsersService();
 
     const user = service.createUser('John', 'john@example.com');
     expect(user.name).toBe('John');
-    expect(user.email).toBe('john@example.com')
-    expect(user.id).toBe(1)
-  })
-
+    expect(user.email).toBe('john@example.com');
+    expect(user.id).toBe(1);
+  });
 
   it('should create two users with diff ids', () => {
-
     const service = new UsersService();
 
     const user = service.createUser('John', 'john@example.com');
-    const user2 = service.createUser("Jane", "jane@example.com");
+    const user2 = service.createUser('Jane', 'jane@example.com');
 
     expect(user.name).toBe('John');
-    expect(user.email).toBe('john@example.com')
-    expect(user.id).toBe(1)
+    expect(user.email).toBe('john@example.com');
+    expect(user.id).toBe(1);
 
-    expect(user2.id).toBe(2)
+    expect(user2.id).toBe(2);
+  });
+
+  it('should the user by id', () => {
+    const service = new UsersService();
+    const user = service.createUser('John', 'john@example.com');
+
+    expect(service.findById(1)).toEqual({
+      id: 1,
+      name: 'John',
+      email: 'john@example.com',
+    });
+  });
+
+  it('should return undefined when user does not exist', () => {
+    const service = new UsersService();
+    const user = service.createUser('John', 'john@example.com');
+
+    expect(service.findById(999)).toBeUndefined();
+  });
+
+  it('should find all users', () => {
+    const service = new UsersService();
+    service.createUser('John', 'john@example.com');
+    service.createUser('John2', 'john2@example.com');
+
+    const users = service.findAll();
+
+    expect(users).toHaveLength(2);
+  });
+
+  it('should delete the user', () => {
+    const service = new UsersService();
+    service.createUser('John', 'john@example.com');
+    service.createUser('John2', 'john2@example.com');
+
+    const deleteResult = service.deleteUser(1);
+
+    expect(deleteResult).toBe(true);
+    expect(service.findById(1)).toBeUndefined();
+  });
+
+  it('should return false when user does not exist for delete', () => {
+
+    const service = new UsersService();
+    service.createUser('John', 'john@example.com');
+
+    const deleteResult = service.deleteUser(999);
+
+    expect(deleteResult).toBe(false)
   })
-  
 });
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,22 +2,36 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-  private users : {
-   id: number;
-   name: string;
-   email: string;
+  private users: {
+    id: number;
+    name: string;
+    email: string;
   }[] = [];
 
   createUser(name: string, email: string) {
     const user = {
-      id: this.users.length +1,
+      id: this.users.length + 1,
       name,
-      email
-    }
+      email,
+    };
     this.users.push(user);
     return user;
   }
-  findById(id: number) {}
-  findAll() {}
-  deleteUser(id: number) {}
+  findById(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+  findAll() {
+    return this.users;
+  }
+  deleteUser(id: number) {
+    const index = this.users.findIndex((user) => user.id === id);
+
+    if (index === -1) {
+      return false;
+    }
+
+    this.users.splice(index, 1);
+
+    return true;
+  }
 }
