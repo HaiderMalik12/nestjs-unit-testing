19b2557c938d289722fb9e71116a0b88bc5d245c
Haider Malik <haidermalik504@gmail.com>
2026-08-18 23:49:49 +0500

    before each and group test cases

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,74 +2,74 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
-  it('should create a new user', () => {
-    const service = new UsersService();
-
-    const user = service.createUser('John', 'john@example.com');
-    expect(user.name).toBe('John');
-    expect(user.email).toBe('john@example.com');
-    expect(user.id).toBe(1);
+  let service: UsersService;
+  beforeEach(() => {
+    service = new UsersService();
   });
+  describe('createUser', () => {
+    it('should create a new user', () => {
+      const user = service.createUser('John', 'john@example.com');
+      expect(user.name).toBe('John');
+      expect(user.email).toBe('john@example.com');
+      expect(user.id).toBe(1);
+    });
+    it('should create two users with diff ids', () => {
+      const user = service.createUser('John', 'john@example.com');
+      const user2 = service.createUser('Jane', 'jane@example.com');
 
-  it('should create two users with diff ids', () => {
-    const service = new UsersService();
+      expect(user.name).toBe('John');
+      expect(user.email).toBe('john@example.com');
+      expect(user.id).toBe(1);
 
-    const user = service.createUser('John', 'john@example.com');
-    const user2 = service.createUser('Jane', 'jane@example.com');
-
-    expect(user.name).toBe('John');
-    expect(user.email).toBe('john@example.com');
-    expect(user.id).toBe(1);
-
-    expect(user2.id).toBe(2);
-  });
-
-  it('should the user by id', () => {
-    const service = new UsersService();
-    const user = service.createUser('John', 'john@example.com');
-
-    expect(service.findById(1)).toEqual({
-      id: 1,
-      name: 'John',
-      email: 'john@example.com',
+      expect(user2.id).toBe(2);
     });
   });
 
-  it('should return undefined when user does not exist', () => {
-    const service = new UsersService();
-    const user = service.createUser('John', 'john@example.com');
+  describe('findById', () => {
+    it('should the user by id', () => {
+      const user = service.createUser('John', 'john@example.com');
 
-    expect(service.findById(999)).toBeUndefined();
+      expect(service.findById(1)).toEqual({
+        id: 1,
+        name: 'John',
+        email: 'john@example.com',
+      });
+    });
+    it('should return undefined when user does not exist', () => {
+      const user = service.createUser('John', 'john@example.com');
+
+      expect(service.findById(999)).toBeUndefined();
+    });
   });
 
-  it('should find all users', () => {
-    const service = new UsersService();
-    service.createUser('John', 'john@example.com');
-    service.createUser('John2', 'john2@example.com');
+  describe('findAll users', () => {
+    it('should find all users', () => {
+      service.createUser('John', 'john@example.com');
+      service.createUser('John2', 'john2@example.com');
 
-    const users = service.findAll();
+      const users = service.findAll();
 
-    expect(users).toHaveLength(2);
+      expect(users).toHaveLength(2);
+    });
   });
 
-  it('should delete the user', () => {
-    const service = new UsersService();
-    service.createUser('John', 'john@example.com');
-    service.createUser('John2', 'john2@example.com');
+  describe('deleteUser', () => {
+    it('should delete the user', () => {
+      service.createUser('John', 'john@example.com');
+      service.createUser('John2', 'john2@example.com');
 
-    const deleteResult = service.deleteUser(1);
+      const deleteResult = service.deleteUser(1);
 
-    expect(deleteResult).toBe(true);
-    expect(service.findById(1)).toBeUndefined();
+      expect(deleteResult).toBe(true);
+      expect(service.findById(1)).toBeUndefined();
+    });
+
+    it('should return false when user does not exist for delete', () => {
+      service.createUser('John', 'john@example.com');
+
+      const deleteResult = service.deleteUser(999);
+
+      expect(deleteResult).toBe(false);
+    });
   });
-
-  it('should return false when user does not exist for delete', () => {
-
-    const service = new UsersService();
-    service.createUser('John', 'john@example.com');
-
-    const deleteResult = service.deleteUser(999);
-
-    expect(deleteResult).toBe(false)
-  })
 });
