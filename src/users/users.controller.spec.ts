2ddcd7f48a1f8318f73785b00434620db567f978
Haider Malik <haidermalik504@gmail.com>
2026-09-01 15:08:06 +0500

    mock user testing

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -4,11 +4,20 @@ import { UsersRepository, UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  const mockUsersService = {
+    findById: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService, UsersRepository],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+        UsersRepository,
+      ],
     }).compile();
     controller = module.get<UsersController>(UsersController);
   });
@@ -17,8 +26,26 @@ describe('UsersController', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should return a user by id', () => {
-    const result = controller.findById('1');
-    expect(result).toEqual({ id: '1' });
+  it('should return a user by id', async () => {
+    mockUsersService.findById.mockResolvedValue({
+      id: '1',
+      name: 'John',
+      email: 'john@example.com',
+    });
+    const result = await controller.findById('1');
+    expect(result).toEqual({
+      id: '1',
+      name: 'John',
+      email: 'john@example.com',
+    });
+
+    expect(mockUsersService.findById).toHaveBeenCalledWith(1);
+  });
+
+  it('should return undefined when user is not found', async () => {
+    mockUsersService.findById.mockResolvedValue(undefined);
+
+    const result = await controller.findById('8888');
+    expect(result).toBeUndefined();
   });
 });
